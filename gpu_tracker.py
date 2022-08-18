71328a3b4beece130b7f78486b46c6bc89862e1c
Centulus <86899297+Centulus@users.noreply.github.com>
2022-08-18 12:57:27 +0200

    Update gpu_tracker.py

--- gpu_tracker.py
+++ gpu_tracker.py
@@ -5,6 +5,13 @@ import os
 from csv import DictWriter
 import requests
 import time
+import gspread
+
+# Tableur en ligne
+
+sa = gspread.service_account()
+sh = sa.open("GPUS")
+wks = sh.worksheet("result")
 
 # Fonction pour bypass CloudFlare
 
